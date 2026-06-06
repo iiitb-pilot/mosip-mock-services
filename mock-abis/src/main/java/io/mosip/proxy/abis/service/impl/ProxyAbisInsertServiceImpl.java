59e67696db3c5d10e45d26ac4842d15467ee8d2d
Nambirajanr <98886358+Nambirajanr@users.noreply.github.com>
2026-06-07 00:14:10 +0530

    Update ProxyAbisInsertServiceImpl.java
    Signed-off-by: Nambirajanr <98886358+Nambirajanr@users.noreply.github.com>

--- mock-abis/src/main/java/io/mosip/proxy/abis/service/impl/ProxyAbisInsertServiceImpl.java
+++ mock-abis/src/main/java/io/mosip/proxy/abis/service/impl/ProxyAbisInsertServiceImpl.java
@@ -170,7 +170,7 @@ public class ProxyAbisInsertServiceImpl implements ProxyAbisInsertService {
 					responseHeader.get("Set-Cookie").get(0).toString().indexOf(";")));
 
 			HttpEntity<String> entity1 = new HttpEntity<String>(headers1);
-			ResponseEntity<String> cbeffResp = restTemplate.exchange(CBEFF_URL, HttpMethod.GET, entity1, String.class).getBody();
+			ResponseEntity<String> cbeffResp = restTemplate.exchange(CBEFF_URL, HttpMethod.GET, entity1, String.class);
 			logger.info("CBEFF response-" + cbeffResp);
 			String cbeff = cbeffResp.getBody();
 			logger.info("CBEFF Data-" + cbeff);
